bc77f49f4b33e9d6862edce065f663cc47d89485
ceflores <ceflores@US-WIN-CJ4OK9EK>
2024-09-13 01:02:09 -0600

    ICON FIXED

--- WebAPISampleApp/MainForm.Designer.cs
+++ WebAPISampleApp/MainForm.Designer.cs
@@ -512,7 +512,7 @@ namespace InSightValidationTool
             this.tabPage2.Location = new System.Drawing.Point(4, 5);
             this.tabPage2.Name = "tabPage2";
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(1906, 959);
+            this.tabPage2.Size = new System.Drawing.Size(1906, 940);
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "+";
             this.tabPage2.UseVisualStyleBackColor = true;
